1edaa122b905ac8087552ff63c57659dd76f91b0
Laura Häkkinen <laura.a.hakkinen@gmail.com>
2022-06-05 14:17:25 +0300

    unicafe muokattu: 1.8

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -4,11 +4,10 @@ const Button = (props) => <button onClick={props.handleClick}>{props.text}</butt
 
 const Display = (props) => <div>{props.text} {props.value}</div>
 
-const App = () => {
-  // tallennetaan napit omaan tilaansa
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+const Statistics = (props) => {
+  const good = props.list[0]
+  const neutral = props.list[1]
+  const bad = props.list[2]
 
   const all = good + neutral + bad
   let avg = ((good * 1) + (neutral * 0) + (bad * -1)) / all
@@ -18,17 +17,32 @@ const App = () => {
 
   return (
     <div>
+      <h1>statistics</h1>
+        <Display text="good" value={good} />
+        <Display text="neutral" value={neutral} />
+        <Display text="bad" value={bad} />
+        <Display text="all" value={all} />
+        <Display text="average" value={avg} />
+        <div>positive {pos} %</div>
+    </div>
+  )
+}
+
+const App = () => {
+  // tallennetaan napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const list = [good, neutral, bad]
+
+  return (
+    <div>
       <h1>give feedback</h1>
       <Button handleClick={() => setGood(good + 1)} text="good" />
       <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
       <Button handleClick={() => setBad(bad + 1)} text="bad" />
-      <h1>statistics</h1>
-      <Display text="good" value={good} />
-      <Display text="neutral" value={neutral} />
-      <Display text="bad" value={bad} />
-      <Display text="all" value={all} />
-      <Display text="average" value={avg} />
-      <div>positive {pos} %</div>
+      <Statistics list={list} />
     </div>
   )
 }

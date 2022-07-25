959fb14e24de07916e3512de915e156e02dd26f3
lauraHakkinen <106445320+lauraHakkinen@users.noreply.github.com>
2022-07-25 17:00:55 +0300

    merge needed

--- osa4/blogilista-backend/controllers/blogs.js
+++ osa4/blogilista-backend/controllers/blogs.js
@@ -1,7 +1,5 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
-const User = require('../models/user')
-const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog
@@ -23,13 +21,7 @@ blogsRouter.get('/:id', async (request, response) => {
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
 
-  const decodedToken = jwt.verify(request.token, process.env.SECRET)
-  if (!request.token || !decodedToken.id) {
-    return response.status(401).json({
-      error: 'token missing or invalid'
-    })
-  }
-  const user = await User.findById(decodedToken.id)
+  const user = request.user
 
   if (!body.title || !body.author || !body.url) {
     return response.status(400).json({
@@ -53,14 +45,7 @@ blogsRouter.post('/', async (request, response) => {
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
-  const decodedToken = jwt.verify(request.token, process.env.SECRET)
-  if (!request.token || !decodedToken.id) {
-    return response.status(401).json({
-      error: 'token missing or invalid'
-    })
-  }
-
-  const user = await User.findById(decodedToken.id)
+  const user = request.user
   const blog = await Blog.findById(request.params.id)
 
   if ( blog.user.toString() === user._id.toString() ) {

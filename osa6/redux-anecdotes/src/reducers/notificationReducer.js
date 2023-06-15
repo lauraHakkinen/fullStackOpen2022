8a66c2d7809ded1d6283c03b1cb1e5dd7faf241d
lauraHakkinen <106445320+lauraHakkinen@users.noreply.github.com>
2023-06-15 14:25:07 +0300

    6.12 done: Notification added

---
+++ osa6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -0,0 +1,10 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const notificationSlice = createSlice({
+  name: 'notification',
+  initialState: 'This is notification',
+  reducers: {
+  }
+})
+
+export default notificationSlice.reducer
